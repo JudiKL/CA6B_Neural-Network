de0e146821a73339add62518851bd394514bfb28
JudiKL <45490444+JudiKL@users.noreply.github.com>
2019-06-12 10:56:08 +0200

    Add files via upload

--- lrCostFunction.m
+++ lrCostFunction.m
@@ -1,51 +1,43 @@
-% function lrCostFunction for EEG Data
-% functional
-
-function [J, grad] = lrCostFunction(theta, data, labels, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+% Title : LRCOSTFUNCTION
+%
+% SUMMARY: Compute cost and gradient for logistic regression with 
 %regularization
+%
+% INPUT: Dataset X, labels y, regularization parameter lambda
+%
+% OUTPUT : J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters.]
+%
+% Made by: Judicaël Fassaya
+% Date: May 12th, 2019
+
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
 % Initialize some useful values
-m = length(labels); % number of training examples
-lambda = 0.1 % to be optimized
-% You need to return the following variables correctly 
+m = length(y); % number of training examples
+
+% returns the following variables
 J = 0;
 grad = zeros(size(theta));
 
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
-%
-%           sigmoid(X * theta)
-%
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
-%
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
+% Computation code : 
 
-g = 1.0 ./ (1 + exp(-data));
+J = ( (1 / m) * sum(-y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta))) ) + (lambda/(2*m))*sum(theta(2:length(theta)).*theta(2:length(theta))) ;
+% compute cost
 
-J = ( (1 / m) * sum(-labels'*log(sigmoid(data*theta)) - (1-labels)'*log( 1 - sigmoid(data*theta))) ) + (lambda/(2*m))*sum(theta(2:length(theta)).*theta(2:length(theta))) ;
 
-grad = (1 / m) * sum((sigmoid(data*theta)-labels).*data);
+% gradent descent formula : 
+grad = (1 / m) * sum((sigmoid(X*theta)-y).*X);
+% compute partial derivative of regularized cost function 
 
 grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
+% regularizes all weights but bias weight
 % =============================================================
 
-grad = grad(:); % reshapes in column the results
+grad = grad(:); % reshapes  the results in column
+
 end
--- oneVsAll.m
+++ oneVsAll.m
@@ -1,73 +1,48 @@
-function [all_theta] = oneVsAll(data, labels, num_labels, lambda)
-%ONEVSALL trains multiple logistic regression classifiers and returns all
-%the classifiers in a matrix all_theta, where the i-th row of all_theta 
-%corresponds to the classifier for label i
-%   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
+% Title : oneVsAll
+%
+% SUMMARY: Implement Logistic Regression to One-vs-all classification
+% Details : [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
 %   logistic regression classifiers and returns each of these classifiers
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
 %   to the classifier for label i
+%
+% INPUT: Matrice : [all examples*all features]
+%
+% OUTPUT:  : Matrice all_theta : [labels*weights]
+%
+% Made by: Judicaël Fassaya
+% Date: May 12th, 2019
 
-    % on our case num_labels = 2
+
+function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 
 % Some useful variables
-m = size(data, 1);
-n = size(data, 2);
+m = size(X, 1);
+n = size(X, 2);
 
-% You need to return the following variables correctly 
+% return the following initialized variable 
 all_theta = zeros(num_labels, n + 1);
 
-% % Add ones to the X data matrix
-% data = [ones(m, 1) data];
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+%
+% Note: fmincg optimizes the cost function and will return theta and the cost 
+% fmincg works similarly to fminunc, but is more efficient when we
+% are dealing with large number of parameters.
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the following code to train num_labels
-%               logistic regression classifiers with regularization
-%               parameter lambda. 
-%
-% Hint: theta(:) will return a column vector.
-%
-% Hint: You can use y == c to obtain a vector of 1's and 0's that tell you
-%       whether the ground truth is true/false for this class.
-%
-% Note: For this assignment, we recommend using fmincg to optimize the cost
-%       function. It is okay to use a for-loop (for c = 1:num_labels) to
-%       loop over the different classes.
-%
-%       fmincg works similarly to fminunc, but is more efficient when we
-%       are dealing with large number of parameters.
-%
-% Example Code for fmincg:
-%
-%     % Set Initial theta
-%     initial_theta = zeros(n + 1, 1);
-%     
-%     % Set options for fminunc
-%     options = optimset('GradObj', 'on', 'MaxIter', 50);
-% 
-%     % Run fmincg to obtain the optimal theta
-%     % This function will return theta and the cost 
-%     [theta] = ...
-%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-%                 initial_theta, options);
-%
-
-%  % Set Initial theta
-% initial_theta = zeros(n + 1, 1);
+ % Set Initial theta
+initial_theta = zeros(n + 1, 1);
  % Set options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 % Run fmincg to obtain the optimal theta
 for c = 1:num_labels
-all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, data, (labels == c), lambda)), initial_theta, options);
-optimal thetas for each row that is to say each class
+all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+% a for-loop (for c = 1:num_labels) to loop over the different classes.
+% output : optimal thetas for each row that is to say each class
 
-% trial with fminunc instead : 
-% 
-%  Set options for fminunc
-% options = optimset('GradObj', 'on', 'MaxIter', 50);
-% computes minimum of lrcost function :
-% [thetas, cost] = ...
-% 	fminunc(@(x)(lrcostFunction(x, data, labels)), initial_theta, options);
+% y == c allows to obtain a vector of 1's and 0's that tell you whether
+% the ground truth is true/false for this class.
 
 
 
